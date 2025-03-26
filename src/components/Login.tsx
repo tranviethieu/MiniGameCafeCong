08058e0e2fbde133a9bc78b07681c4dbd262478d
DESKTOP-3NBEK92\TranVietHieu <hieutran26498@gmail.com>
2025-03-26 14:24:49 +0700

    1234

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,19 +1,15 @@
-import { useContext, useState } from "react";
-import { message } from "antd";
+import { useContext } from "react";
+import { Form, Input, Button, message, ConfigProvider } from "antd";
 import { db, doc, getDoc, setDoc } from "../lib/firebaseConfig";
 import { IUserContext, UserContext } from "../context/UserProvider";
 import { motion } from "framer-motion";
+
 const validateVietnamesePhoneNumber = (phone: string): boolean => {
   const vietnamPhoneRegex = /^(?:\+84|0)(?:\d{9}|\d{8})$/;
   return vietnamPhoneRegex.test(phone);
 };
-interface LoginProps {
-  setShowLogin: (show: boolean) => void;
-}
 
-const Login: React.FC<LoginProps> = () => {
-  const [phone, setPhone] = useState("");
-  const [name, setName] = useState("");
+const Login: React.FC = () => {
   const { setUser, loading, setLoading } =
     useContext<IUserContext>(UserContext);
 
@@ -25,7 +21,9 @@ const Login: React.FC<LoginProps> = () => {
     return formattedPhone;
   };
 
-  const handleLogin = async () => {
+  const handleLogin = async (values: { phone: string; name?: string }) => {
+    const { phone, name } = values;
+
     if (phone === "0123456789" && name === "admin") {
       setLoading(true);
       setUser({
@@ -44,12 +42,13 @@ const Login: React.FC<LoginProps> = () => {
       message.success(`Đăng nhập admin!`);
       return;
     }
-    if (!phone) return message.error("Vui lòng nhập số điện thoại!");
+
     if (!validateVietnamesePhoneNumber(phone)) {
       return message.error(
         "Số điện thoại không hợp lệ! Vui lòng nhập số điện thoại Việt Nam."
       );
     }
+
     const formattedPhone = formatPhoneNumber(phone);
     setLoading(true);
 
@@ -80,7 +79,7 @@ const Login: React.FC<LoginProps> = () => {
           setLoading(false);
           return;
         }
-        await setDoc(userRef, {
+        const userData = {
           phone: formattedPhone,
           name,
           level: 1,
@@ -91,75 +90,75 @@ const Login: React.FC<LoginProps> = () => {
           linkb4: "",
           linkb5: "",
           giftCode: "",
-        });
-        setUser({
-          phone: formattedPhone,
-          name,
-          level: 1,
-          status: 1,
-          linkb1: "",
-          linkb2: "",
-          linkb3: "",
-          linkb4: "",
-          linkb5: "",
-          giftCode: "",
-        });
+        };
+        await setDoc(userRef, userData);
+        setUser(userData);
         message.success("Tài khoản mới đã được tạo!");
       }
     } catch (error: any) {
       message.error("Lỗi khi kiểm tra dữ liệu: " + error.message);
     }
     setLoading(false);
-  }; ///update
+  };
 
   return (
-    <motion.div
-      initial={{ opacity: 0, x: -100 }}
-      animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0, x: 100 }}
-      transition={{ duration: 0.5 }}
-      className="flex items-center justify-center min-h-screen bg-gray-200"
-      style={{
-        backgroundImage: `url("https://cong-news.appwifi.com/wp-content/uploads/2019/05/11copy.jpg")`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
+    <ConfigProvider
+      theme={{
+        components: {
+          Form: {
+            labelColor: "#fff",
+          },
+        },
       }}
     >
-      <div className="w-full  max-w-md p-6 bg-gray-700 rounded-lg shadow-xl border border-gray-700 ">
-        <h2 className="text-white text-2xl font-bold text-center mb-4 animate-pulse">
-          Login Mini Game
-        </h2>
-        <div className="mb-4">
-          <label className="block text-gray-300 mb-1">
-            Tên (chỉ nhập nếu đăng ký mới)
-          </label>
-          <input
-            type="text"
-            className="w-full p-2 border border-gray-600 rounded bg-gray-700 text-white focus:ring-2 focus:ring-blue-400 transition-all duration-300"
-            placeholder="Nhập tên của bạn"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
+      <motion.div
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, x: 100 }}
+        transition={{ duration: 0.5 }}
+        className="flex items-center justify-center min-h-screen bg-gray-200"
+        style={{
+          backgroundImage: `url("https://cong-news.appwifi.com/wp-content/uploads/2019/05/11copy.jpg")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <div className="w-full max-w-md p-6 bg-gray-700 rounded-lg shadow-xl border border-gray-700">
+          <h2 className="text-white text-2xl font-bold text-center mb-4 animate-pulse">
+            Login Mini Game
+          </h2>
+          <Form layout="vertical" onFinish={handleLogin}>
+            <Form.Item
+              label="Tên (chỉ nhập nếu đăng ký mới)"
+              name="name"
+              style={{ fontWeight: 600 }}
+            >
+              <Input placeholder="Nhập tên của bạn" />
+            </Form.Item>
+            <Form.Item
+              label="Số điện thoại"
+              name="phone"
+              rules={[
+                { required: true, message: "Vui lòng nhập số điện thoại!" },
+              ]}
+              style={{ fontWeight: 600 }}
+            >
+              <Input placeholder="Nhập số điện thoại" />
+            </Form.Item>
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="w-full"
+                loading={loading}
+              >
+                {loading ? "Đang xử lý..." : "🎮 Đăng nhập / Đăng ký"}
+              </Button>
+            </Form.Item>
+          </Form>
         </div>
-        <div className="mb-4">
-          <label className="block text-gray-300 mb-1">Số điện thoại</label>
-          <input
-            type="text"
-            className="w-full p-2 border border-gray-600 rounded bg-gray-700 text-white focus:ring-2 focus:ring-blue-400 transition-all duration-300"
-            placeholder="Nhập số điện thoại"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-          />
-        </div>
-        <button
-          className="w-full p-3 bg-blue-600 hover:bg-blue-500 text-white font-bold rounded transition-all duration-300 transform hover:scale-105"
-          onClick={handleLogin}
-          disabled={loading}
-        >
-          {loading ? "Đang xử lý..." : "🎮 Đăng nhập / Đăng ký"}
-        </button>
-      </div>
-    </motion.div>
+      </motion.div>
+    </ConfigProvider>
   );
 };
 

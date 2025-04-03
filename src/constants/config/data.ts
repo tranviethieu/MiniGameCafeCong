b9081b9f6c2027762031ff27265e14f3e4ee8392
HIEUTRAN\hieutv.pc <Hieutran26498@gmail.com>
2025-04-03 22:10:06 +0700

    ff

--- src/constants/config/data.ts
+++ src/constants/config/data.ts
@@ -13,25 +13,25 @@ export const taskGame: {
   {
     key: 2,
     name: "Nhiệm vụ 2",
-    timeStart: "04/04/2025 00:00:00",
+    timeStart: "04/04/2025 09:00:00",
     timeEnd: "04/20/2025 00:00:00",
   },
   {
     key: 3,
     name: "Nhiệm vụ 3",
-    timeStart: "04/04/2025 00:00:00",
+    timeStart: "04/04/2025 12:00:00",
     timeEnd: "04/20/2025 00:00:00",
   },
   {
     key: 4,
     name: "Nhiệm vụ 4",
-    timeStart: "04/04/2025 00:00:00",
+    timeStart: "05/05/2025 10:00:00",
     timeEnd: "04/20/2025 00:00:00",
   },
   {
     key: 5,
     name: "Nhiệm vụ 5",
-    timeStart: "04/04/2025 00:00:00",
+    timeStart: "06/06/2025 00:00:00",
     timeEnd: "04/20/2025 00:00:00",
   },
 ];

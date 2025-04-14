a0f4ff2f7e858fd4251760a4ccceb87931ac08dd
HIEUTRAN\hieutv.pc <Hieutran26498@gmail.com>
2025-04-14 23:11:25 +0700

    1

--- src/pages/auth/StartGameCong/StartGameCong.tsx
+++ src/pages/auth/StartGameCong/StartGameCong.tsx
@@ -1,19 +1,18 @@
 import { motion } from "framer-motion";
 import { ArrowCircleRight2 } from "iconsax-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import LoginCong from "../LoginCong";
 import icons from "~/constants/images/icons";
 
 const StartGameCong = () => {
   const [showLogin, setShowLogin] = useState(false);
-  // useEffect(() => {
-  //   const playVideo = () => {
-  //     const video = document.getElementById("videocong") as HTMLVideoElement;
-  //     if (video) video.play().catch(() => {});
-  //   };
-  //   window.addEventListener("touchstart", playVideo, { once: true });
-  //   return () => window.removeEventListener("touchstart", playVideo);
-  // }, []);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowLogin(true);
+    }, 5000); // 3 giây
+
+    return () => clearTimeout(timeout); // clear timeout nếu component unmount
+  }, []);
 
   return !showLogin ? (
     <div
@@ -21,6 +20,7 @@ const StartGameCong = () => {
         backgroundImage: `url(${icons.anhNen})`,
       }}
       className="w-full bg-cover bg-center bg-no-repeat min-h-screen font-[Cousine] text-[17px] bg-[#e4e3d9] flex flex-col"
+      onClick={() => setShowLogin(true)}
     >
       <div className="flex flex-col flex-1">
         <div className="flex justify-center mt-2">
@@ -37,7 +37,6 @@ const StartGameCong = () => {
           className="flex justify-center"
         >
           <img
-            onClick={() => setShowLogin(true)}
             src={icons.vietnamtrongcong} // ảnh nền bạn muốn ở dưới
             alt="ảnh nền"
             className="w-full max-w-[500px] object-contain"
@@ -48,11 +47,10 @@ const StartGameCong = () => {
           animate={{ opacity: 1 }}
           transition={{ duration: 1.5, delay: 2 }}
           className="mb-2 flex justify-center"
-          onClick={() => setShowLogin(true)}
         >
           <button
             type="button"
-            //onClick={() => setShowLogin(true)}
+            onClick={() => setShowLogin(true)}
             className="bg-[#4c5b29] font-[Cousine] px-6 py-[6px] text-[#e7e5db] rounded-full flex items-center justify-center gap-2 transition"
           >
             <span>THAM GIA NGAY</span>

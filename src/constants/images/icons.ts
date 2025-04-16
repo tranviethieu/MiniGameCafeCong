dd7bbb873f64d63b5e287079998d7a5067c30689
DESKTOP-3NBEK92\TranVietHieu <hieutran26498@gmail.com>
2025-04-16 15:34:55 +0700

    update cộng giảm dung lương

--- src/constants/images/icons.ts
+++ src/constants/images/icons.ts
@@ -1,29 +1,21 @@
 import logoCong from "/static/logoCong.png";
-import bgCong from "/static/bgCong.png";
-import quizResult from "/static/quiz-result-bg.svg";
 import hgc from "/static/anh1.png";
 import miniGame from "/static/miniGame.png";
 import muiten from "/static/muiten.png";
-import bgVideo from "/static/video/0410.mov";
 import anhCho from "/static/imgs/anhcong.jpg";
 import vietnamtrongcong from "/static/imgs/vietnamtrongcong.webp";
 import anhNen from "/static/imgs/anhNen.webp";
 import xemay from "/static/imgs/xemay.png";
-import vietnam from "/static/imgs/vietnam.gif";
 import videocong from "/static/video/videocong.mp4";
 
 export default {
   logoCong,
-  quizResult,
-  bgCong,
   hgc,
   miniGame,
   muiten,
-  bgVideo,
   anhCho,
   vietnamtrongcong,
   anhNen,
   xemay,
-  vietnam,
   videocong,
 };
